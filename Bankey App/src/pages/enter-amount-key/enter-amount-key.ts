5ccfa7baee02420c01601b479d5c537f4acab032
pranav887 <pranav.patwardhan.com>
2018-01-18 14:16:42 +0530

    Date picker issue resolved

--- Bankey App/src/pages/enter-amount-key/enter-amount-key.ts
+++ Bankey App/src/pages/enter-amount-key/enter-amount-key.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {PersonalDetailKeyPage} from "../personal-detail-key/personal-detail-key";
-
+import { StripeServiceProvider } from "../../providers/stripe-service/stripe-service"
 /**
  * Generated class for the EnterAmountKeyPage page.
  *
@@ -15,8 +15,16 @@ import {PersonalDetailKeyPage} from "../personal-detail-key/personal-detail-key"
   templateUrl: 'enter-amount-key.html',
 })
 export class EnterAmountKeyPage {
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ExpiryDate:any;
+  card = {
+   number: '',
+   expMonth:"",
+   expYear:"",
+   cvc: ''
+  };
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public stripeService:StripeServiceProvider) {
   }
 
   ionViewDidLoad() {
@@ -24,6 +32,16 @@ export class EnterAmountKeyPage {
   }
 
   GoPersonalDetailKey(){
+    console.log(this.ExpiryDate);
+    if(this.ExpiryDate){
+      this.card["expMonth"] = this.ExpiryDate.split("-")[1];
+      this.card["expYear"] = this.ExpiryDate.split("-")[0];
+    }
+    this.stripeService.createCardTokend(this.card).then((result)=>{
+      console.log(result);
+    },(err)=>{
+      console.log(err);
+    });
     this.navCtrl.push(PersonalDetailKeyPage);
   }
   goBack(){
